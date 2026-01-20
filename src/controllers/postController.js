3a75e78b344564797252bdbf6f657c906bc22c2b
Amit Mark <amitmark100@gmail.com>
2026-01-20 18:45:43 +0200

    adding create post functionality

---
+++ src/controllers/postController.js
@@ -0,0 +1,15 @@
+const Post = require('../models/postModel');
+const Comment = require('../models/commentModel');
+
+async function addPost(req, res) {
+  try {
+    const { title, content, sender } = req.body;
+    if (!title || !content || !sender) {
+      return res.status(400).json({ error: 'title, content and sender are required' });
+    }
+    const created = await Post.create({ title, content, sender });
+    res.status(201).json(created);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const db = require('./config/db');
+const postRoutes = require('./routes/postRoutes');
 
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
+app.use('/post', postRoutes);
+;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Posts & Comments API' });
+});
 
 const PORT = process.env.PORT || 3000;
 
